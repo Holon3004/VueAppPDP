0622a8cfa408e445fbad81c59825bac9d06949af
0x00610x006C <i.m.allesssandro@gmail.com>
2023-04-22 20:26:22 +0300

    added new components in app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <main-panel>
   </main-panel>
+  <little-square :x="30" :y="35"/>
+  <big-square :x="30" :y="110"/>
 </template>
 
 <script>
 import MainPanel from "@/components/Main";
+import LittleSquare from "@/components/LittleSquare";
+import BigSquare from "@/components/BigSquare";
+
 export default {
   name: 'App',
-  components: {MainPanel,
+  components: {
+    BigSquare,
+    LittleSquare, MainPanel,
   }
 }
 </script>
