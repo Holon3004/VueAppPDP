d1fcd4fa1e22040a5409b7f1e900c078abf17a4b
0x00610x006C <i.m.allesssandro@gmail.com>
2023-04-22 20:25:59 +0300

    crate template and props

--- src/components/BigSquare.vue
+++ src/components/BigSquare.vue
@@ -1,10 +1,17 @@
 <template>
-
+  <rect
+      style="fill:#000000;stroke:#000000;stroke-width:1"
+      id="rect925"
+      width="75"
+      height="80"
+      x={{y}}
+      y={{x}} />
 </template>
 
 <script>
 export default {
-  name: "BigSquare"
+  name: "BigSquare",
+  props: ['x', 'y']
 }
 </script>
 
--- src/components/LittleSquare.vue
+++ src/components/LittleSquare.vue
@@ -1,10 +1,17 @@
 <template>
-
+  <rect
+      style="fill:#0000ff;stroke:#0000ff;stroke-width:1"
+      id="rect923"
+      width="40"
+      height="40"
+      x={{y}}
+      y={{x}} />
 </template>
 
 <script>
 export default {
-  name: "LittleSquare"
+  name: "LittleSquare",
+  props: ['x', 'y']
 }
 </script>
 
