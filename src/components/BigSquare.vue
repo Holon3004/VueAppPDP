097fa14c6a201dfb6dc32a2063953e456b2d572d
Holon3004 <107874161+Holon3004@users.noreply.github.com>
2023-05-12 01:25:33 +0300

    objects located on points from props v2 (early version not work because developer blind sheep)

--- src/components/BigSquare.vue
+++ src/components/BigSquare.vue
@@ -1,28 +1,46 @@
 <template>
   <svg
-    width="75"
-    height="80">
+      class="u_svg"
+      width="75"
+      height="75">
     <rect
-          id="rect925"
-          class="rect925"
-          width="75"
-          height="80"
-          x=0
-          y=0 />
+        id="rect925"
+        class="rect925"
+        width="75"
+        height="75"
+        x=0
+        y=0 />
   </svg>
 </template>
 
 <script>
 export default {
   name: "BigSquare",
-  props: ['x', 'y']
+  props: ['x', 'y'],
+  data(){
+    let x_s = this.x + "px";
+    let y_s = this.y + "px";
+        document.body.style.setProperty("--pos-b-x", x_s),
+        document.body.style.setProperty("--pos-b-y", y_s)
+  }
 }
 </script>
 
 <style scoped>
+
+:root{
+  --pos-b-x:0px;
+  --pos-b-y:0px;
+}
 .rect925{
   fill:#000000;
   stroke:#000000;
-  stroke-width:1
+  stroke-width:1;
 }
+.u_svg{
+  position: absolute;
+  left: var(--pos-b-x);
+  top: var(--pos-b-y);
+}
+
 </style>
