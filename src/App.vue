ff63ce6db042dd9c8bf8142db8cf99a366129983
Holon3004 <107874161+Holon3004@users.noreply.github.com>
2023-05-12 23:14:19 +0300

    САНЯЯЯЯЯЯЯЯ УУУУУУУУУУУ

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,51 @@
 <template>
   <little-square :x="30" :y="35"/>
-  <big-square :x="300" :y="150"/>
+  <big-square :x="30" :y="15"/>
   <little-square :x="300" :y="350"/>
+  <little-square :x="340" :y="350"/>
+  <little-square :x="380" :y="350"/>
+  <little-square :x="300" :y="390"/>
+  <little-square :x="300" :y="430"/>
+  <little-square :x="300" :y="470"/>
+  <little-square :x="300" :y="510"/>
+  <little-square :x="340" :y="510"/>
+  <little-square :x="380" :y="510"/>
+
+  <little-square :x="450" :y="350"/>
+  <little-square :x="490" :y="350"/>
+  <little-square :x="530" :y="350"/>
+  <little-square :x="450" :y="390"/>
+  <little-square :x="450" :y="430"/>
+  <little-square :x="450" :y="470"/>
+  <little-square :x="450" :y="510"/>
+  <little-square :x="530" :y="390"/>
+  <little-square :x="530" :y="430"/>
+  <little-square :x="530" :y="470"/>
+  <little-square :x="530" :y="510"/>
+  <little-square :x="490" :y="470"/>
+
+  <little-square :x="600" :y="350"/>
+  <little-square :x="600" :y="390"/>
+  <little-square :x="600" :y="430"/>
+  <little-square :x="600" :y="470"/>
+  <little-square :x="600" :y="510"/>
+  <little-square :x="640" :y="470"/>
+  <little-square :x="680" :y="350"/>
+  <little-square :x="680" :y="390"/>
+  <little-square :x="680" :y="430"/>
+  <little-square :x="680" :y="470"/>
+  <little-square :x="680" :y="510"/>
+
+  <little-square :x="750" :y="390"/>
+  <little-square :x="750" :y="430"/>
+  <little-square :x="750" :y="510"/>
+  <little-square :x="790" :y="350"/>
+  <little-square :x="790" :y="470"/>
+  <little-square :x="830" :y="350"/>
+  <little-square :x="830" :y="390"/>
+  <little-square :x="830" :y="430"/>
+  <little-square :x="830" :y="470"/>
+  <little-square :x="830" :y="510"/>
 
 </template>
 
